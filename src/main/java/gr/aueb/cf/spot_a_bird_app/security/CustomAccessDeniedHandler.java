a097f52d8f6ec897c08dfacf845ff52ce8685ec2
evaperisteri <evangelia.peristeri.g@gmail.com>
2025-07-26 21:33:51 +0300

    feat: added security

---
+++ src/main/java/gr/aueb/cf/spot_a_bird_app/security/CustomAccessDeniedHandler.java
@@ -0,0 +1,24 @@
+package gr.aueb.cf.spot_a_bird_app.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
+
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException)
+            throws IOException {
+
+        // Set the response status and content type
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setContentType("application/json;charset=UTF-8");
+
+        // Write a custom JSON response with the collected information
+        String jsonResponse = "{\"code\": \"userNotAuthorized\", \"description\": \"User is not allowed to visit this route.\"}";
+        response.getWriter().write(jsonResponse);
+    }
+}
