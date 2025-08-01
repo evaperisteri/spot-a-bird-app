810f50e63ff8385ae08407bc15a503d6a6a19474
evaperisteri <evangelia.peristeri.g@gmail.com>
2025-08-01 21:21:10 +0300

    feat: user rest updated

--- src/main/java/gr/aueb/cf/spot_a_bird_app/rest/UserRestController.java
+++ src/main/java/gr/aueb/cf/spot_a_bird_app/rest/UserRestController.java
@@ -24,6 +24,7 @@ import java.util.List;
 public class UserRestController {
 
     private final UserService userService;
+
     @PostMapping("/users/save")
     public ResponseEntity<UserReadOnlyDTO> saveUser(@Valid UserInsertDTO userInsertDTO,
            BindingResult bindingResult)
