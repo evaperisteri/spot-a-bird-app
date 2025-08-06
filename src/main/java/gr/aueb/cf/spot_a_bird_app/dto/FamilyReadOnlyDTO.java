494b1ade7cb5cb65780bd1d10689b2bdc8364752
evaperisteri <evangelia.peristeri.g@gmail.com>
2025-08-06 15:18:14 +0300

    feat: updates

--- src/main/java/gr/aueb/cf/spot_a_bird_app/dto/FamilyReadOnlyDTO.java
+++ src/main/java/gr/aueb/cf/spot_a_bird_app/dto/FamilyReadOnlyDTO.java
@@ -1,5 +1,14 @@
 package gr.aueb.cf.spot_a_bird_app.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class FamilyReadOnlyDTO {
 
     Long id;
