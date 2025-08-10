c2d02e2686c6020dbc03eb12b0603e4fc20b1661
evaperisteri <evangelia.peristeri.g@gmail.com>
2025-08-10 20:46:41 +0300

    feat: userUpdateDTO added

---
+++ src/main/java/gr/aueb/cf/spot_a_bird_app/dto/UserUpdateDTO.java
@@ -0,0 +1,32 @@
+package gr.aueb.cf.spot_a_bird_app.dto;
+
+import gr.aueb.cf.spot_a_bird_app.core.enums.Gender;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Data
+public class UserUpdateDTO {
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @Email(message = "Enter a correct email")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @Past(message = "Date of birth must be in the past")
+    private LocalDate dateOfBirth;
+
+    private Gender gender;
+}
