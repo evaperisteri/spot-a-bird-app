05ce656f1b97b48d6dd3497c5f8298a41abd9d67
evaperisteri <evangelia.peristeri.g@gmail.com>
2025-08-10 17:58:09 +0300

    feat: filterRegions method added

--- src/main/java/gr/aueb/cf/spot_a_bird_app/rest/RegionRestController.java
+++ src/main/java/gr/aueb/cf/spot_a_bird_app/rest/RegionRestController.java
@@ -1,11 +1,14 @@
 package gr.aueb.cf.spot_a_bird_app.rest;
 
 import gr.aueb.cf.spot_a_bird_app.dto.RegionReadOnlyDTO;
+import gr.aueb.cf.spot_a_bird_app.model.Region;
+import gr.aueb.cf.spot_a_bird_app.repository.RegionRepository;
 import gr.aueb.cf.spot_a_bird_app.service.RegionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,10 +19,23 @@ import java.util.List;
 public class RegionRestController {
 
     private final RegionService regionService;
+    private final RegionRepository regionRepository;
 
     @GetMapping
     public ResponseEntity<List<RegionReadOnlyDTO>> getAllRegions() {
         List<RegionReadOnlyDTO> regions = regionService.getAllRegions();
         return ResponseEntity.ok(regions);
+    }
+
+    @GetMapping("/filter")
+    public ResponseEntity<List<RegionReadOnlyDTO>> filterRegions(@RequestParam String name) {
+
+        List<Region> regions = regionRepository.findByNameContainingIgnoreCase(name);
+
+        return ResponseEntity.ok(
+                regions.stream()
+                        .map(r -> new RegionReadOnlyDTO(r.getId(), r.getName()))
+                        .toList()
+        );
     }
 }
