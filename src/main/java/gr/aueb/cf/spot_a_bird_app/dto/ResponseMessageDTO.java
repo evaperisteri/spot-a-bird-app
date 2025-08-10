c6c9557128dc60c01bfc275b2e9b73184d5c86b3
evaperisteri <evangelia.peristeri.g@gmail.com>
2025-08-10 17:58:50 +0300

    fix: no args constructor added

--- src/main/java/gr/aueb/cf/spot_a_bird_app/dto/ResponseMessageDTO.java
+++ src/main/java/gr/aueb/cf/spot_a_bird_app/dto/ResponseMessageDTO.java
@@ -2,8 +2,10 @@ package gr.aueb.cf.spot_a_bird_app.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class ResponseMessageDTO {
     private String code;
