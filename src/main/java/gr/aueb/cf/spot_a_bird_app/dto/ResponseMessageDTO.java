8014e5bbfdfb806662a7249bbde1a80cbe8cc6b6
evaperisteri <evangelia.peristeri.g@gmail.com>
2025-08-10 17:59:42 +0300

    fix: no args constructor removed

--- src/main/java/gr/aueb/cf/spot_a_bird_app/dto/ResponseMessageDTO.java
+++ src/main/java/gr/aueb/cf/spot_a_bird_app/dto/ResponseMessageDTO.java
@@ -2,10 +2,8 @@ package gr.aueb.cf.spot_a_bird_app.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 public class ResponseMessageDTO {
     private String code;
