7e17bcef0968713f69d04052d27904c8d4ed4573
evaperisteri <evangelia.peristeri.g@gmail.com>
2025-08-12 19:38:19 +0300

    feat: statsController updated

--- src/main/java/gr/aueb/cf/spot_a_bird_app/rest/StatsRestController.java
+++ src/main/java/gr/aueb/cf/spot_a_bird_app/rest/StatsRestController.java
@@ -1,10 +1,11 @@
 package gr.aueb.cf.spot_a_bird_app.rest;
 
+import gr.aueb.cf.spot_a_bird_app.core.exceptions.AppObjectNotFoundException;
 import gr.aueb.cf.spot_a_bird_app.dto.stats.BirdStatisticsDTO;
 import gr.aueb.cf.spot_a_bird_app.dto.stats.UserLogStatisticsDTO;
+import gr.aueb.cf.spot_a_bird_app.service.StatsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,14 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/stats")
 @RequiredArgsConstructor
 public class StatsRestController {
-    private final StatisticsService statisticsService;
+    private final StatsService statisticsService;
     @GetMapping("/birds")
     public ResponseEntity<BirdStatisticsDTO> getBirdStatistics() {
         return ResponseEntity.ok(statisticsService.getBirdStatistics());
     }
 
     @GetMapping("/user-logs")
-    public ResponseEntity<UserLogStatisticsDTO> getUserLogStatistics() {
+    public ResponseEntity<UserLogStatisticsDTO> getUserLogStatistics() throws AppObjectNotFoundException {
         return ResponseEntity.ok(statisticsService.getUserLogStatistics());
     }
 }
