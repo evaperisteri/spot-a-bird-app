98c117b78d7a5208eb5ad59600c96d6108558884
evaperisteri <evangelia.peristeri.g@gmail.com>
2025-08-13 17:17:04 +0300

    fix: constructors

--- src/main/java/gr/aueb/cf/spot_a_bird_app/dto/stats/FamilyCountDTO.java
+++ src/main/java/gr/aueb/cf/spot_a_bird_app/dto/stats/FamilyCountDTO.java
@@ -1,23 +1,17 @@
 package gr.aueb.cf.spot_a_bird_app.dto.stats;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class FamilyCountDTO {
     private Long familyId;
     private String familyName;
-    private long birdCount;
-    private long observationCount;
-
-    public FamilyCountDTO(Long id, String name, Long birdCount, Long observationCount) {
-        this.familyId = id;
-        this.familyName = name;
-        this.birdCount = birdCount;
-        this.observationCount = observationCount;
-    }
+    private Long birdCount;
+    private Long observationCount;
 }
